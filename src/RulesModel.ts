f86f024368b43fb2a97cc7238818e9312525aa5e
Damien GILLES <pro@gillesdamien.com>
2018-03-12 23:29:24 +0100

    removed src

--- src/RulesModel.ts
+++
@@ -1,8 +0,0 @@
-import { Papers, OptionalPaper } from './PapersModel';
-
-export interface Rule {
-    text: string,
-    errorMessage(papers: Papers): string,
-    validate(papers: Papers): boolean,
-    papersUsed: Array<OptionalPaper>
-}
